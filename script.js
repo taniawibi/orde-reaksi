2feb5dc0d3172a94946040edb1599e10cf5c15e9
taniawibi <taniawibisana3@gmail.com>
2026-01-03 13:07:36 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function hitung() {
+    let t = parseFloat(document.getElementById("t").value);
+    let a0 = parseFloat(document.getElementById("a0").value);
+    let at = parseFloat(document.getElementById("at").value);
+
+    // Orde 1
+    let k1 = (Math.log(a0 / at)) / t;
+
+    // Orde 2
+    let k2 = ((1 / at) - (1 / a0)) / t;
+
+    document.getElementById("hasil").innerHTML =
+        "Konstanta laju Orde 1 (k₁): " + k1.toFixed(4) +
+        "<br>Konstanta laju Orde 2 (k₂): " + k2.toFixed(4);
+}
